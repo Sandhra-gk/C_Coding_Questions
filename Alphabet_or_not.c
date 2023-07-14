24dae80c6272e2859d533a757304fda5669637d5
Sandhra GireeshKumar <79519899+Sandhra-gk@users.noreply.github.com>
2023-07-14 15:52:10 +0530

    Create Alphabet_or_not.c

---
+++ Alphabet_or_not.c
@@ -0,0 +1,17 @@
+// Check if the entered character is an alphabet or not
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char input;
+    printf("Enter input: ");
+    scanf("%c", &input);
+    
+    if((input >= 'a' && input <= 'z') || (input >= 'A' && input <= 'Z'))
+    printf("Alphabet");
+    else
+    printf("Not an alphabet");
+    return 0;
+}
